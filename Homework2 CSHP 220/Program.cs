fb582390881998fa67ba9548a5622ab901164a0a
Rick Jackson <richja01@noa.nintendo.com>
2020-04-24 21:48:47 -0700

    Added homework2

---
+++ Homework2 CSHP 220/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_CSHP_220
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+            users.Add(new Models.User { Name = "Ron", Password = "super" });
+            users.Add(new Models.User { Name = "Frank", Password = "hello" });
+            users.Add(new Models.User { Name = "Cindy", Password = "cindy" });
+
+            var newUsers = users.Where(t => t.Password == "hello").ToList();
+            Console.WriteLine("These users have hello as thier password:");
+
+            foreach (var user in newUsers)
+            {
+                Console.WriteLine("Name = {0} Password = {1}", user.Name, user.Password);
+            }
+
+            Console.WriteLine();
+
+            users.RemoveAll(t => t.Password == t.Name.ToLower());
+            Console.WriteLine("Password as name has been removed:");
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("Name = {0} Password = {1}", user.Name, user.Password);
+            }
+
+            Console.WriteLine();
+
+            newUsers.Remove(newUsers.First());
+            Console.WriteLine("The first user to have hello as their password has been deleted:");
+
+            foreach (var user in newUsers)
+            {
+                Console.WriteLine("Name = {0} Password = {1}", user.Name, user.Password);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
